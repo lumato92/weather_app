99a21650bc10cba6f92ad39b66a5563b7d1f0740
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-06-02 22:09:43 -0300

    Version basica
    Se agrega search bar, se carga iconos e info basica (temp , min. max)

--- scripts/request.js
+++ scripts/request.js
@@ -1,47 +1,66 @@
 const API_KEY= "032d722ccabf2ab54d2ff584d7f74e09";
+var KELVIN = 273.15
+
+let btnSearch = document.getElementById("searchID");
 
 let btnLocation = document.getElementById("btn-location");
-let btnWeather = document.getElementById("btn-weather")
+// let btnWeather = document.getElementById("btn-weather");
 
-let URL_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=032d722ccabf2ab54d2ff584d7f74e09`
+// let URL_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=032d722ccabf2ab54d2ff584d7f74e09`
 
 let nombre = "lucas"
 
 async function getLocation() {
     
-    let city = document.getElementById("city").value
+    let city = document.getElementById("city-search").value
     let URL_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=032d722ccabf2ab54d2ff584d7f74e09`
     const response = await fetch(URL_API);
     const data = await response.json();
     
-    document.getElementById("lat-value").innerText = data[0].lat;
-    document.getElementById("lon-value").innerText = data[0].lon;
+
+    getWeather(data[0].lat, data[0].lon);
 
 
-    // return data;
-    // lon = data[0].lon;
 }
 
- async function getWeather(){
-    let lat = document.getElementById("lat-value").textContent;
-    let lon = document.getElementById("lon-value").textContent;
+ async function getWeather(lat, lon){
 
+    console.log("aca ok")
     let URL_API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=032d722ccabf2ab54d2ff584d7f74e09`
     const response = await fetch(URL_API);
     const data = await response.json();
     
     console.log(data.weather[0].description);
+    console.log(data);
 
-
-}
-
-function test() {
-    // let nombre = "lucas;"
-    console.log(`sos rancio ${nombre}`)
-    console.log(document.getElementById("city").value)
+    infoUpdate(data);
     
 }
 
-btnLocation.addEventListener("click",getLocation)
-btnWeather.addEventListener("click",getWeather)
+
+
+const infoUpdate = (data)=>{
+    
+    document.getElementById("city-name").innerText = data.name;
+    document.getElementById("cur-temp").innerText = Math.round(data.main.temp - KELVIN);
+    document.getElementById("max-temp").innerText = Math.round(data.main.temp_max - KELVIN);
+    document.getElementById("min-temp").innerText = Math.round(data.main.temp_min - KELVIN);
+    
+    document.getElementById("info-descr").innerText=data.weather[0].description
+   
+   
+    document.getElementById("stat-icon").style.backgroundImage=`url(http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`
+    document.getElementById("stat-icon").style.backgroundRepeat="no-repeat";
+    document.getElementById("stat-icon").style.backgroundSize="cover";
+
+
+
+
+
+}
+
+const test = ()=> { console.log("aca ok")}
+
+
+btnSearch.addEventListener("click", getLocation);
 
